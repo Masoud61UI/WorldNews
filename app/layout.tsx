5373d2f281c75555ae4d24b96b007e7f02fa1ea3
Masoud61UI <masoud6181@gmail.com>
2024-06-13 20:36:27 +0330

    add local font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const dana = localFont({
+  src: "../public/font/DanaVF.woff2",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html dir="rtl" lang="fa-Ir">
+      <body className={`${dana.className}`}>{children}</body>
     </html>
   );
 }
